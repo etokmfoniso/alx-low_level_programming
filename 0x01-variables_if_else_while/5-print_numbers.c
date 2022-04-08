75e297f0571fd4e01cb99d9f31aaa6b45a2c2d6c
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-08 16:03:00 -0700

    single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int a;
+
+for (a = 0; a < 10; a++)
+{
+printf("%i", a);
+}
+
+return (0);
+
+}
