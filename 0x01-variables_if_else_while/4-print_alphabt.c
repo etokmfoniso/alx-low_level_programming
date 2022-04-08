e1234b789747b84fd1c974d500ad90911ebd4ef5
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-08 15:49:14 -0700

    my continue task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(0)
+
+{
+char lower_case;
+
+for (lower_case = 'a', lower_case <= 'z'. lower_case++)
+{
+if (lower_case == 'e')
+continue;
+
+else if (lower_case == 'q')
+continue;
+
+putchar(lower_case);
+}
+
+putchar('\n');
+
+return (0);
+
+}
