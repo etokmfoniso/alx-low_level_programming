a0737e112f7441e283d629aa41c31602a2479800
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-08 16:25:40 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correction)
+ */
+int main(void)
+
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+
+putchar('\n');
+
+return (0);
+
+}
