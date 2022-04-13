211c33c5a16ffce75f7e09cbdb9f72bb90ddc8cb
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-12 18:50:40 -0700

    new

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchaf(k + '0');
+				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
@@ -32,6 +32,6 @@ void times_table(void)
 				_putchar(k % 10 + '0');
 			}
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
