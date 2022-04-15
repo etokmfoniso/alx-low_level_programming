f6426cb9c9cb89fe120288fa8c9ec9bc4cc9cfac
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-15 11:03:10 -0700

    task three

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 - 9
+ *
+ * Return: always 0
+ */
+void print_numbers(void)
+{
+int i;
+
+i = 0;
+
+for (i = o; i < 10; i++)
+
+_putchar(i);
+
+_putchar('\n');
+}
