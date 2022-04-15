77905233fd66f2c8005072bec2bc9d3220d13a64
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-15 08:35:50 -0700

    task zero

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * -isupper.c - checks uppercase
+ * @c: prints integer
+ * Return: give 1 0r 0
+ */
+int -isupper.c(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
