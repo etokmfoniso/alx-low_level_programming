9a6b3f988cddae90b9c10612dbc2bc38c9dd4ca7
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-15 17:11:05 -0700

    update

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,6 +10,7 @@ void print_diagonal(int n)
 int i, j;
 
 if (n > 0)
+
 {
 for (i = 0; 1 < n; i++)
 {
@@ -17,12 +18,16 @@ for (j = 0; j < 1; j++)
 {
 _putchar(' ');
 }
+
 _putchar('\\');
 _putchar('\n');
+
 }
 }
+
 else
 {
 _putchar('\n');
 }
+
 }
