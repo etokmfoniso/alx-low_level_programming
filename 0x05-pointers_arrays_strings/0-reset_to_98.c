e82527f22ac987fec807866be5a9cd331991f794
etokmfoniso <etokmfoniso@yahoo.com>
2022-04-20 11:26:57 -0700

    assigning integer to a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * reset_to_98 - updates an integer value
+ * @n: a pointer to an integer that will updated
+ *
+ * Return: void that is correct
+ */
+void reset_to_98(int *n)
+{
+
+*n = 98;
+
+}
