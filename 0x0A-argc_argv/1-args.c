4deec177a7a4c2a87069cb99e6381cf4422e18a0
etokmfoniso <etokmfoniso@yahoo.com>
2022-05-04 04:54:10 -0700

    task one

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - program to count number of arguments 
+ * @argc: number of arg
+ * @argv: an array of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+printf("number of arguments = %d\n", argc);
+
+return (0);
+}
