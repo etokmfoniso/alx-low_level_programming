72d8dd9ec6f7d810cc1074f9ec7b630dcb24d728
etokmfoniso <etokmfoniso@yahoo.com>
2022-05-04 07:15:06 -0700

    updated task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,11 +7,17 @@
  * Return: Always 0
  */
 
-int main(int argc , char *argv[])
+int main(int argc , char **argv)
 {
 int i;
 
+if (argc > 0)
+{
 for (i = 0; i <= argc; i++)
-	printf("%s\n", argv[i]);
+{
+printf("%s\n", argv[i]);
+}
+}
+
 return (0);
 }
