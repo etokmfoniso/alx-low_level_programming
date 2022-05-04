d0de1a4c4df148d4f56e9c09ef8f5e1ce4a92588
etokmfoniso <etokmfoniso@yahoo.com>
2022-05-04 05:44:41 -0700

    updated task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - program to count number of arguments 
+ * main - program to count number of arguments
  * @argc: number of arg
  * @argv: an array of arguments
  * Return: always 0
