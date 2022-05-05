e52642cae85c8c698bf95d6132b05dfc039d1eb1
etokmfoniso <etokmfoniso@yahoo.com>
2022-05-05 12:29:47 -0700

    task four

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 if (grid != NULL && height != 0)
 {
 	for (; height >= 0; height--)
-		free (grid[height]);
-	free (grid);
+		free(grid[height]);
+	free(grid);
 }
 }
