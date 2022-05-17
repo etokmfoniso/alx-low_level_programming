3b792be0e3fbdaa87a8c2b14894641b0553c9828
etokmfoniso <etokmfoniso@yahoo.com>
2022-05-17 14:46:59 -0700

    task one

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,7 +5,7 @@
  * print_numbers - a function that prints all the numbers
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function 
- *
+ * @...: variable numbers to be printed
  * Return: returns nothing
  */
 
@@ -18,9 +18,10 @@ va_start(args, n);
 
 for (i = 0; i < n; i++)
 {
-printf("%d", va_arg(args, int));
-if (i != (n - 1) && seperator != NULL)
-	printf("%s", seperator);
+	printf("%d", va_arg(args, int));
+
+	if (i != (n - 1) && separator != NULL)
+		printf("%s", separator);
 }
 printf("\n");
 
